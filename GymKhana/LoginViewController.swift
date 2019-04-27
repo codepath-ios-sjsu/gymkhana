705ea9b460e0e3e37729c1b74f7eb6ae08f4e7eb
Cagan Sevencan <cagansvncn@gmail.com>
2019-04-26 23:56:03 -0700

    WIP

---
+++ GymKhana/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  GymKhana
+//
+//  Created by Cagan Sevencan on 4/26/19.
+//  Copyright © 2019 bargemayur05@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
